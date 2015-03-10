bd7f9b67c405be2f6e905224331f251cae3d9f2c
Adham Helal <adham.helal@gmail.com>
2015-03-10 14:02:52 +0100

    Changed the key layout and can now expand keys so that it would be easier merged

--- library/usersdb.py
+++ library/usersdb.py
@@ -7,61 +7,72 @@ class UsersDB(object):
         self.users_db = self.module.params["usersdb"]
         self.teams_db = self.module.params["teamsdb"]
         self.servers_db = self.module.params["serversdb"]
-        self.compiled_list = []
+        self.expanded_users_db = []
+
+    @staticmethod
+    def merge_user(user_definition, user_items):
+        print user_definition
+        print user_items
+        return dict(user_definition.items() + user_items.items())
 
     def expand_servers(self):
         # Expand server will overwrite same attributes defined in userdb except for state = "absent"
         for a_user in self.servers_db:
             # 1st lets get the user/team dictionary from the userdb
-
-            #team_definition = self.users_db.get("team")
-            if a_user.get("user"):
-                user_definition = self.users_db.get(a_user.get("user"))
-                if user_definition.get("state", None) == "absent":
+            user_definition = a_user.get("user") or a_user.get("name", False)
+            team_definition = a_user.get("team", False)
+            if user_definition:
+                if user_definition.get("state", None) in ("absent", "delete", "deleted", "remove", "removed"):
+                    # Don't merge you will delete any way
                     pass
-                    # dont merge you will delete any way
                 else:
                     # merge do some magic
-                    # TODO: How to merge Keys and key options 
-                    a_user = dict(user_definition.items() + a_user.items())
+                    a_user = self.merge_user(user_definition.items(), a_user.items())
 
-            elif a_user.get("team"):
+            elif team_definition:
+                # Should expand teams
                 pass
             else:
                 self.module.fail_json(msg="Your server definition has no user or team. Please check your data type.")
             # Add the final merge
-            self.compiled_list.append(a_user)
+            self.expanded_users_db.append(a_user)
+
+    def expand_keys(self, keys, user):
+        if len(keys) == 0:
+            # TODO: Should work without keys maybe ?!?!?
+            self.module.fail_json(msg="user '{}' has no keys defined.".format(user))
+
+        user_keys = []
+        # If key is not a list than its a raw key string
+        if not isinstance(keys, list):
+            user_keys.append({"key": keys})
+        else:
+            for key in keys:
+                # Basic syntax check
+                if isinstance(key, basestring) and "ssh-" in key:
+                    user_keys.append({"key": key})
+                elif "key" not in key and "name" not in key:
+                    self.module.fail_json(msg="user '{}' list has no keys defined.".format(key.keys()))
+                else:
+                    # All is okay just add the dict
+                    user_keys.append(key)
+
+        return user_keys
 
     def expand_users(self):
+        # Get User database which is a dic i.e.
+        # daniels:
+        #     comment : 'Jack daniels'
+        #     keys     :
+
         for a_user, a_user_options in self.users_db.iteritems():
+            # 1- Convert dic to list (servers_db style)
             a_user = {"name": a_user}  # create the account name
             a_user.update(a_user_options)  # update all other option
-            # Get pubkey
-            a_user_key = a_user_options.get("pubkey", [])
-            if len(a_user_key) == 0:
-                self.module.fail_json(msg="user '{}' has no key defined.".format(a_user))
-
-            compiled_keys = []
-            if not isinstance(a_user_key, list):
-                a_user_key = [a_user_key]
-
-            for key in a_user_key:
-                user_key = {}
-                if isinstance(key, dict):
-                    if key.get("state", None):
-                        user_key.update({"state": key.get("state")})
-                    if key.get("key_options", None):
-                        user_key.update({"key_options": key.get("key_options")})
-                    if key.get("pubkey", None):
-                        user_key.update({"pubkey": key.get("pubkey")})
-                else:
-                    # just key
-                    user_key.update({"pubkey": key})
-
-                compiled_keys.append(user_key)
-                a_user.update({"keys": compiled_keys})
-            self.compiled_list.append(a_user)
-
+            # 2- Compile key
+            a_keys = a_user_options.get("keys", [])
+            a_user.update({"keys": self.expand_keys(a_keys, a_user)})
+            self.expanded_users_db.append(a_user)
 
     def main(self):
         if self.servers_db:
@@ -70,7 +81,7 @@ class UsersDB(object):
         else:
             # Simple just expand the user list
             self.expand_users()
-        result = {"changed": False, "msg": "", "compiled_list": self.compiled_list}
+        result = {"changed": False, "msg": "", "expanded_users_db": self.expanded_users_db}
 
         self.module.exit_json(**result)
 
