be802c9067c9fa0d90aca47998f9b450fbabe358
Adham Helal <adham.helal@gmail.com>
2015-03-13 21:08:15 +0100

    Kitchen testworking

--- test/run_kitchen.sh
+++ test/run_kitchen.sh
@@ -6,10 +6,10 @@ echo "* mkdir /kitchen"
 mkdir /kitchen
 
 echo "* cp -ar /mnt/shared /kitchen"
-cp -r /mnt/shared/. /kitchen/
+cp -r /mnt/shared/. /kitchen
 
 echo "* cd /kitchen"
-cd /kitchen
+cd /kitchen/*
 
 echo "* python test/travis_run.py"
 python test/travis_run.py
