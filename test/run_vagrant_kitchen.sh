facaaf3764c2204ef51cf574425ba9b58a15dafd
Adham Helal <adham.helal@gmail.com>
2015-04-16 17:55:26 +0200

    test kitchen verify could run multi. times

--- test/run_vagrant_kitchen.sh
+++ test/run_vagrant_kitchen.sh
@@ -3,12 +3,12 @@ set -e
 echo "**** Box setup ***"
 
 echo "* mkdir /kitchen"
-mkdir /kitchen
+mkdir -p /kitchen
 
 #echo "* cp -ar /mnt/shared /kitchen"
 #cp -r /mnt/shared/. /kitchen
-echo "* ln -s /mnt/shared /kitchen"
-ln -s /mnt/shared/* /kitchen/
+echo "* ln -sf /mnt/shared /kitchen"
+ln -sf /mnt/shared/* /kitchen/
 
 echo "* cd /kitchen"
 cd /kitchen/*
