fea8cb8b559f851197b32ad1e47d8d8793fc1b6d
Till Klocke <till.klocke@gmail.com>
2015-08-08 16:18:48 +0200

    It should now be possible to not fail on rogue users

--- library/check_users.py
+++ library/check_users.py
@@ -14,6 +14,7 @@ class CheckUsers(object):
   def __init__(self, module):
     self.module = module
     self.cusers = self.module.params["users_var"]
+    self.fail_on_error = self.module["fail_on_error"]
 
   def main(self):
     users = pwd.getpwall()
@@ -26,7 +27,7 @@ class CheckUsers(object):
 
       if (uid > 999 and self.isValidShell(shell)):
         if not self.isUserNameInDb(name):
-          self.module.fail_json(msg="User {} not defined by ansible".format(name))
+          self.exitWithResult("User {} not defined by ansible".format(name))
         else:
           self.checkUsersKeys(user)
       count = count + 1
@@ -56,7 +57,7 @@ class CheckUsers(object):
         break
 
     if usersKeys == None:
-      self.module.fail_json(msg="User {} has no ansible defined SSH keys".format(user.pw_name))
+      self.exitWithResult("User {} has no ansible defined SSH keys".format(user.pw_name))
 
     installed_keys = self.loadInstalledAuthorizedKeys(user)
     configured_keys = []
@@ -65,23 +66,30 @@ class CheckUsers(object):
 
     for key in installed_keys:
       if not key in configured_keys:
-        self.module.fail_json(msg="User {} has rogue ssh authorized keys".format(user.pw_name))
+        self.exitWithResult("User {} has rogue ssh authorized keys".format(user.pw_name))
 
 
   def loadInstalledAuthorizedKeys(self, user):
     authorized_keys_file = os.path.join(user.pw_dir,'.ssh','authorized_keys')
     if not os.path.isfile(authorized_keys_file):
-      self.module.fail_json(msg="Can't load authorized keys from {} for user {}".format(authorized_keys_file, user.pw_name))
+      self.exitWithResult("Can't load authorized keys from {} for user {}".format(authorized_keys_file, user.pw_name))
     with open(authorized_keys_file, 'r') as infile:
       data = infile.read()
     authorized_keys = data.splitlines()
     return authorized_keys
+
+  def exitWithResult(self, message):
+    if self.fail_on_error:
+      self.module.fail_json(msg=message)
+    else:
+      self.module.exit_json(msg=message)
     
 
 def main():
   module = AnsibleModule(
       argument_spec=dict(
           users_var=dict(default=None, required=True, type='dict')
+          fail_on_error=dict(default=True, required=False, type='bool')
       ),
       supports_check_mode=False
   )
