046d123387824e675229f5d8d8154790d2163a3f
Renat S. Nurgaliyev <impleman@gmail.com>
2017-02-24 12:55:20 +0100

    Add verbosity

--- test/integration/include-vars-dir/serverspec/test_spec.rb
+++ test/integration/include-vars-dir/serverspec/test_spec.rb
@@ -2,8 +2,11 @@ require_relative '../../helper_spec.rb'
 
 ansible_version=`ansible --version`
 
+puts "ANSIBLE_VERSION=", ansible_version
+puts "AND THE THING IS=", ansible_version[/ansible 2.[01]./,].nil?puts
+
 unless ansible_version[/ansible 2.[01]./,].nil?
-  
+
   describe group('group1') do
     it { should exist }
   end
